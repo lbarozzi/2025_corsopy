b26366fffd275a410b61f8bc653bd17b37638d3e
Leonardo Barozzi <leonardo.barozzi@gmail.com>
2025-03-18 10:17:10 +0100

    test

--- 01_example.py
+++ 01_example.py
@@ -1,17 +1,34 @@
 import os
+import tempfile
+import os.path
 import requests
 import json
 
 http_url="https://random-word-api.herokuapp.com/word"
 
 try:
-    res= requests.get(http_url)
+    p={"number":"10"}
+    res= requests.get(http_url, params=p)
     res.raise_for_status()
+
     print(f"{res.status_code} - {res.reason}")
     words = json.loads(res.text)
 
     for w in words:
         print(w)
+
+    tmp_file= open("wordlist.txt", "a") # default "r" #"w" write #"x" create #"a" append
+    for w in words:
+        tmp_file.write(w + "\n")
+
+    tmp_file.close()
+
+    with tempfile.NamedTemporaryFile(delete=False, mode="w") as tmp_file:
+        for w in words:
+            tmp_file.write(w + "\n")
+        tmp_file.flush()
+        print(f"Temp file created : {tmp_file.name}")
+
 
 except requests.exceptions.RequestException as ops:
     print(f"Error getting data from {http_url} : {ops}")
